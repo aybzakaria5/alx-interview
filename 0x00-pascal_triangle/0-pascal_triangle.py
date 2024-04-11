baf86818fcdb5c80a9f8859a1fe30f519dd22606
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-11 21:16:11 +0000

    that pascal tirnagle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""""Pascal's Triangle"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
