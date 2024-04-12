cd55c70b5efb7bf03274b6cb7ddf4b3b568c7f2f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-12 22:36:36 +0000

    merging

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """"Pascal's Triangle"""
 
+
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal's triangle of nn"""
     if n <= 0:
         return []
 
