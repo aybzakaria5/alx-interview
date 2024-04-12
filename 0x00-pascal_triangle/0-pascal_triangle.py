cfd1f1ad94c9e83ce7746677e244480aba3b197f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-04-12 16:12:38 +0000

    fixing the pep8 validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,8 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal's triangle of n"""
+    """Returns a list of lists of integers representing *
+    the Pascal's triangle of n"""
     if n <= 0:
         return []
 
