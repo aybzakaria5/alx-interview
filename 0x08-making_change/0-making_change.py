763c94228bc13cd0616c4acd20304b053ea5c636
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-06-09 22:15:24 +0000

    some fixing

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-"""Change making module.
+"""
+Main file for testing
 """
 
 
 def makeChange(coins, total):
-    """Finds the minimum number of coins needed to achieve a given total.
-
-    Args:
-        coin_values (list): The values of the coins available.
-        target_amount (int): The target amount to reach.
-
-    Returns:
-        int: The minimum number of coins needed to reach the total,
-             or -1 if it's not possible.
     """
-    if total <= 0:
+    Determines the minimum number of coins needed to achieve the target amount.
+    """
+    if total < 1:
         return 0
 
-    # Sort coin values in descending order to start
-    # with the largest denominations
-    sorted_coin_values = sorted(coin, reverse=True)
-    remaining_amount = total
-    total_coins = 0
+    coins.sort(reverse=True)
+    coin_count = 0
 
-    for coin in sorted_coin_values:
-        while remaining_amount >= coin:
-            remaining_amount -= coin
-            total_coins += 1
-        if remaining_amount == 0:
+    for value in coins:
+        if total == 0:
             break
-
-    # If loop exits and there is remaining amount
-    # we cannot meet the target amount
-    if remaining_amount > 0:
+        max_coins = total // value
+        total -= max_coins * value
+        coin_count += max_coins
+    if total != 0:
         return -1
-
-    return total_coins
+    return coin_count
